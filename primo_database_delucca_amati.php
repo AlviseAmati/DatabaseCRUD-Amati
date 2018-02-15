47d9a303c95df82821927dbab4bcf32cd1ddf79a
Delli2000 <manuel.delucca22@gmail.com>
2018-02-15 12:02:29 +0100

    Inizio 1

--- primo_database_delucca_amati.php
+++ primo_database_delucca_amati.php
@@ -2,12 +2,39 @@
 $servername = "localhost";
 $username = "username";
 $password = "password";
+$dbname = "myDB";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//////      INSERT
+$sql = "INSERT INTO DatabaseCRUD-Amati-Delucca (nome, cognome, email, numero di telefono, indirizzo, città, CAP, Codice Fiscale)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+///////
+///         DELETE
+$sql = "DELETE FROM DatabaseCRUD-Amati-Delucca WHERE id=3";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+///         UPDATE
+ $sql = "UPDATE DatabaseCRUD-Amati-Delucca SET lastname='Doe' WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+///
+$conn->close();
